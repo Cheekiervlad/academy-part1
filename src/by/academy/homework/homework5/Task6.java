74c8bfbd6ec5dc711903f571fd4401dd7da0d16e
Vlad <Vlad.-96@mail.ru>
2021-03-07 19:39:54 +0300

    homework5

---
+++ src/by/academy/homework/homework5/Task6.java
@@ -0,0 +1,21 @@
+package by.academy.homework.homework5;
+
+import java.util.Random;
+
+public class Task6 {
+
+    private static final Random random = new Random();
+    public static final int INITIAL_SIZE = 8;
+    public static final int AMOUNT_OF_ELEMENTS = 10;
+
+    public static void main(String[] args) {
+        int[] array = new int[INITIAL_SIZE];
+        try {
+            for (int i = 0; i < AMOUNT_OF_ELEMENTS; i++) {
+                array[i] = random.nextInt();
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array is to small, expand the array");
+        }
+    }
+}
